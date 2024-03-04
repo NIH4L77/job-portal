b0fab9f08619518a7d0a2f2294dc41cb46092019
Nihal Shaikh <nihaljamilshaikh77@gmail.com>
2024-03-04 16:20:06 +0530

    1st changes

---
+++ src/component/LoginPage.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './LoginPage.css'
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = () => {
+    // Check username and password
+    if (username === 'admin' && password === 'password') {
+      // Successful login, redirect or perform action
+      alert('Login Successful!');
+    } else {
+      // Failed login, display error message
+      setErrorMessage('Invalid username or password');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default LoginPage;
